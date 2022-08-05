457d4785ddccf13f5072e9576fcfd6c0a9eb2dd2
Keerthana0902 <svjk0902@gmail.com>
2022-08-05 05:37:44 +0000

    Question03 updated

--- SQL_Challenge/question03.sql
+++ SQL_Challenge/question03.sql
@@ -1,21 +1,33 @@
-CREATE TEMPORARY TABLE ds2 as
-	SELECT DISTINCT DATE as Date, 
-    ROUND(ABS(Close - Open), 2) as Diff_Range from sample_dataset3
-    ORDER BY Diff_Range DESC limit 3;
-
-DROP TEMPORARY TABLE ds2 ;
-
-SELECT* from ds2;
-
-CREATE TEMPORARY TABLE ds3 as
-	SELECT DATE, MAX(High) as maxHigh from sample_dataset3 WHERE
-    DATE in (SELECT DATE from ds2)
-    GROUP BY DATE;
+DELIMITER //
+CREATE PROCEDURE Trading_range ()
+BEGIN
+	CREATE TEMPORARY TABLE IF NOT EXISTS Date_Range as
+		SELECT DISTINCT DATE as Date, 
+		ROUND(ABS(Close - Open), 2) as Diff_Range from sample_dataset3
+		ORDER BY Diff_Range DESC limit 3;
+	 
+    CREATE TEMPORARY TABLE IF NOT EXISTS MaxHigh as
+		SELECT DATE, MAX(High) as maxHigh from sample_dataset3 WHERE
+		DATE in (SELECT DATE from Date_Range)
+		GROUP BY DATE;
+		
+	CREATE TEMPORARY TABLE IF NOT EXISTS DateTimeMax as
+		SELECT sample_dataset3.Date, 
+        Date_format(str_to_date(sample_dataset3.Time, '%H%i'), '%H:%i') as Max_Time from sample_dataset3 
+		JOIN MaxHigh on sample_dataset3.High = MaxHigh.maxHigh AND 
+        sample_dataset3.Date = MaxHigh.Date
+		GROUP BY MaxHigh.Date;
+		
+	CREATE TEMPORARY TABLE IF NOT EXISTS Result as
+		SELECT Date_Range.Date, Diff_Range, Max_Time 
+		from Date_Range 
+		JOIN DateTimeMax on DateTimeMax.Date = Date_Range.Date;
+	  
+	SELECT* FROM Result;
     
-DROP TEMPORARY TABLE ds3 ;
+	DROP TEMPORARY TABLE MaxHigh ;
+	DROP TEMPORARY TABLE DateTimeMax ;
+	DROP TEMPORARY TABLE Date_Range ;
+    DROP TEMPORARY TABLE Result;
     
-SELECT* from ds3;
-
-CREATE TEMPORARY TABLE ds4 as
-	SELECT Date, Time from sample_dataset3 JOIN
-    ds3 on DATE;
+END //
